264b9de75536eb5588b90a09519fc8726c70597b
daniel <dpletzke@gmail.com>
2021-01-25 22:25:55 -0600

    initial setup

---
+++ src/pages/register.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return <div>Register Page</div>;
+};
+
+export default Register;
