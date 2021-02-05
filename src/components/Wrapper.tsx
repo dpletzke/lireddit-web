21d9900bb7da2ad28a9ce086173d22f58fddaa65
daniel <dpletzke@gmail.com>
2021-02-05 00:03:08 -0600

    make small variant wrapper

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -10,7 +10,12 @@ export const Wrapper: React.FC<WrapperProps> = ({
   variant = "regular",
 }) => {
   return (
-    <Box mt={8} mx="auto" maxW={variant ? "800px" : "400px"} w="100%">
+    <Box
+      mt={8}
+      mx="auto"
+      maxW={variant === "regular" ? "800px" : "400px"}
+      w="100%"
+    >
       {children}
     </Box>
   );
