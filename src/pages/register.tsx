708317257d9f3a5858163e1c9c2c0c1d7013f59a
daniel <dpletzke@gmail.com>
2021-02-04 23:58:58 -0600

    work on chakra front-end

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { Formik, Form } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  HStack,
+  Radio,
+  FormHelperText,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import { formatWithValidation } from "next/dist/next-server/lib/utils";
+import { Wrapper } from "../components/Wrapper";
 
 interface registerProps {}
 
 export const Register: React.FC<registerProps> = ({}) => {
-  return <div>Register Page</div>;
+  return (
+    <Wrapper>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(values, handleChange) => (
+          <FormControl id="first-name" isRequired>
+            <FormLabel>Username</FormLabel>
+            <Input
+              value={values.username}
+              onChange={handleChange}
+              placeholder="User Name"
+              id="username"
+              size="sm"
+            />
+          </FormControl>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 export default Register;
